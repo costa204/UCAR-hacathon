3ecfa7cb18b3260c9dfc5138dd243791a79baf22
youssef <majdoubyoussef79@gmail.com>
2026-04-25 19:57:21 +0100

    intial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'uCAR | University Interface',
+    year: '2026' 
+  });
+});
+
+module.exports = router;
